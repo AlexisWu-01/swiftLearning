663f432db4e05b3fbbcd67cc4875f6a3feacf1a3
AlexisWu <wuxinyi2000@gmail.com>
2022-01-05 18:38:56 +0800

    update

--- notes/struct.md
+++ notes/struct.md
@@ -1,2 +1,100 @@
 # Struct
-# Method and Functions
+##  Method vs Functions
+- methods belong to a type
+    - eg: structs, enums, classes
+- Both can accpet parameters and return values
+
+- **Method advantage** 
+    - avoid namespace pollution
+
+## Marking Methods as Mutating
+A method not marked as mutating cannot make changes to struct properties (even it's `var`!)
+
+---
+## Computed property
+- Stored property
+- computed property: 
+    - updated every time you use it
+    - between methods and variables
+- To use it or not?
+    - depends on if this property depends on another data
+- get/ set function
+- `newValue` swift built in function (in `set`)
+- constants cannot be computed properties
+
+## Property Observers
+- takes actions whenever property changes
+- Use it with a `didSet` and `oldValue`; `willSet` and `newValue`
+```
+struct App{
+    var contacts = [String](){
+        willSet{
+            print("Current value is \(contacts)")
+            print("New value will be \(newValue)")
+        }
+        didSet{
+            print("There are now \(contacts.count) contacts")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+```
+
+- Why use it?
+    - executed every time you change a property
+    - Don't use it if you are including slow work
+
+- We use `didSet` more brcause we want to take actions after the changes like update interface, save changes, etc.
+
+- We also use `willSet` to know the state of the program before change is made.
+
+---
+
+## Initializer
+- special functions inside structs
+- to prepare for a new struct
+- `init` does not start with `func`
+
+- If you want to have both the default and custom initializer:
+```
+struct Employee{
+    var name: String
+    var yearsActive = 0
+}
+
+extension Employee{
+    init(){
+        self.name = "Anonymous"
+        print("Creating an anonymous employee")
+    }
+}
+
+#So that both will work:
+let roslin = Employee(name:"Laura Roslin")
+
+let anon = Employee()
+```
+
+## Self Method
+- refers to a current instance of a struct
+- required for accessing self inside a closure that belongs to a class.
+
+---
+## Limit Access to Internal Data w/ **Access Control**
+- types
+    - `private`: Don't let anything outside the struct use this.
+    - `fileprivate`: Don't let anything outside the current file use this.
+    - `public`: let anyone, anywhere use this.
+    - `private(set)`: public read, inside method write.
+- put `types above` before `var`
+- Swift cannot generate memberwise initializers for private properties **even with default value!**
+- Use custom init when access control is present
+- Why use it?
+    1. help developers control what they can do
+    2. good for other people to know which property should not be changed.
+
+---
+## Static properties and methods
+- adding methods and properties to struct itself but not instances.
+
+
--- struct.playground/Contents.swift
+++ struct.playground/Contents.swift
@@ -1,3 +1,151 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Employee{
+    let name: String
+    var vacationAllocated = 14
+    var vacationtaken = 0
+    
+    var vacationRemaining: Int{
+        get{vacationAllocated-vacationtaken}
+        set{
+            vacationAllocated = vacationtaken + newValue
+        }
+    }
+}
+
+var archer = Employee(name: "Stering Archer",vacationAllocated: 14)
+//archer.vacationtaken += 4
+//archer.vacationRemaining = 5
+//print(archer.vacationAllocated)
+//archer.vacationtaken += 4
+//print(archer.vacationRemaining)
+
+
+struct City {
+    var population: Int
+    var description: String {
+        if population < 100_000 {
+            return "This is a small city."
+        } else if population < 1_000_000 {
+            return "This is a medium-sized city."
+        } else {
+            return "This is a large city."
+        }
+    }
+}
+let tokyo = City(population: 12_000_000)
+
+//print(tokyo.description)
+
+
+struct Game{
+    var score = 0{
+        didSet{
+            print("Score is now \(score) compared to \(oldValue)!")
+        }
+    }
+}
+
+var game = Game()
+//game.score += 10
+
+struct App{
+    var contacts = [String](){
+        willSet{
+            print("Current value is \(contacts)")
+            print("New value will be \(newValue)")
+        }
+        didSet{
+            print("There are now \(contacts.count) contacts")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+//app.contacts.append("1")
+//app.contacts.append("2")
+//app.contacts.append("3")
+
+
+struct Player{
+    let name: String
+    let number: Int
+    
+    init(name:String){
+        self.name = name
+        self.number = Int.random(in:1...3)
+    }
+}
+
+let player = Player(name:"Magan K")
+//print(player.number)
+
+
+struct Country {
+    var name: String
+    var usesImperialMeasurements: Bool
+    init(countryName: String) {
+        name = countryName
+        let imperialCountries = ["Liberia", "Myanmar", "USA"]
+        if imperialCountries.contains(name) {
+            usesImperialMeasurements = true
+        } else {
+            usesImperialMeasurements = false
+        }
+    }
+}
+let country = Country(countryName: "USA")
+//country.name
+//country.usesImperialMeasurements
+
+
+struct BankAccount{
+    private(set) var funds = 0
+    mutating func deposit(amount: Int){
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool{
+        if funds > amount{
+            funds -= amount
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+//
+//if success{
+//    print("Yay")
+//}
+//else{
+//    print("oops, too poor")
+//}
+struct School {
+    var staffNames: [String]
+    private var studentNames: [String]
+    init(staff: String...) { //"..." means there will be an array of that object
+        self.staffNames = staff
+        self.studentNames = [String]()
+    }
+}
+let royalHigh = School(staff: "Mrs Hughes")
+
+
+struct School1{
+    static var StudentCount = 0
+    
+    static func add(student: String){
+        print("\(student) joined")
+        StudentCount += 1
+    }
+}
+
+School1.add(student:"Mani Fok")
+print(School1.StudentCount)
